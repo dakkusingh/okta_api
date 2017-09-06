23e94c991895ffb269d07071e31c240082fbb569
dakku <dakku@97634.no-reply.drupal.org>
2017-09-06 15:05:49 +0100

    Adding first draft of settings form.

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\okta_api\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Admin form for Okta API settings.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'okta_api_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'okta_api.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormTitle() {
+    return 'Okta API Settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('okta_api.settings');
+
+    $form['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#description' => $this->t('The API key to use.'),
+      '#default_value' => $config->get('api_key'),
+    ];
+
+    $form['organisation_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your Okta domain'),
+      '#description' => $this->t('The the domain your organisation uses to log into Okta'),
+      '#default_value' => $config->get('organisation_url'),
+    ];
+
+    $form['default_group_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Default group ID'),
+      '#description' => $this->t('The default group id to add the user to in Okta'),
+      '#default_value' => $config->get('default_group_id'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $this->config('okta_api.settings')
+      ->set('api_key', $form_state->getValue('api_key'))
+      ->set('default_group_id', $form_state->getValue('default_template_id'))
+      ->set('organisation_url', $form_state->getValue('organisation_url'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
