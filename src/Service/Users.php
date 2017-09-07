95f7d88ad40b0e815d665a5b35cab5e3ea91d0db
dakku <dakku@97634.no-reply.drupal.org>
2017-09-07 08:45:58 +0100

    Adding skeleton code to extend user methods.

--- src/Service/Users.php
+++ src/Service/Users.php
@@ -31,14 +31,8 @@ class Users {
     // TODO This seems outdated in the OKTA API documentation.
     // Check API and update.
     /*
-    $profile = new UserProfile();
-
-    $profile->setFirstName($first_name)
-      ->setLastName($last_name)
-      ->setLogin($email_address)
-      ->setEmail($email_address);
-
-    $user->setProfile($profile);
+    // TODO Set profile here or somewhere else?
+    $user = userSetProfile($first_name, $last_name, $email_address, $user)
 
     $user->setGroupIds([
       $config->get('default_group_id'),
@@ -47,9 +41,38 @@ class Users {
     return $user->create() ? TRUE : FALSE;
   }
 
+  // TODO Extend the CRUD
+  /*public function userSave($user) {
+    return $user->save() ? TRUE : FALSE;
+  }*/
+
   public function userGetByEmail($email_address) {
     $user = new User();
     return $user->get($email_address);
   }
+
+  // TODO Extend the CRUD
+  //public function userUpdate($something) {}
+
+  // TODO Extend the CRUD
+  //public function userDelete($something) {}
+
+  // TODO Extend the Profile
+  //public function userGetProfile($something) {}
+
+  // TODO Extend the Profile
+  /*public function userSetProfile($first_name, $last_name, $email_address, $user) {
+    $profile = new UserProfile();
+
+    $profile->setFirstName($first_name)
+      ->setLastName($last_name)
+      ->setLogin($email_address)
+      ->setEmail($email_address);
+
+    $user->setProfile($profile);
+    return $user;
+  }*/
+
+
 
 }
