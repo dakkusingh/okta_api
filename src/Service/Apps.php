02c45425a18bece2a1f1f12418d2bfa9e49309ae
Stuart Carruthers <stuartcarruthers@3543145.no-reply.drupal.org>
2017-09-11 12:08:44 +0100

    Added Apps service

---
+++ src/Service/Apps.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\okta_api\Service;
+
+use Okta\Exception;
+use Okta\Resource\App;
+
+/**
+ * Service class for Okta apps.
+ */
+class Apps
+{
+    protected $apps;
+
+    /**
+     * Apps constructor
+     *
+     * @param \Drupal\okta_api\Service\OktaClient $oktaClient - An OktaClient.
+     */
+    public function __construct(OktaClient $oktaClient) 
+    {
+        $this->apps = new App($oktaClient->Client);
+    }
+
+    /**
+     * Gets all Okta apps.
+     *
+     * @return object
+     *   A list of Okta apps.
+     */
+    public function getAllApps() 
+    {
+        try {
+            return $this->apps->get('');
+        }
+        catch (Exception $e){
+            // TODO: Handle exceptions.
+        }
+    }
+}
