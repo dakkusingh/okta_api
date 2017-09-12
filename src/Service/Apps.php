d0862fb1492c44932bb939fbd2971edd71b9554a
Stuart Carruthers <stu@ironiclensflare.com>
2017-09-12 17:17:20 +0100

    Add user to application

--- src/Service/Apps.php
+++ src/Service/Apps.php
@@ -36,4 +36,44 @@ class Apps {
     }
   }
 
+  /**
+   * Gets a single Okta app by its ID.
+   *
+   * @param string $appId
+   *   The Okta app ID.
+   *
+   * @return object
+   *   The Okta app.
+   */
+  public function getAppById($appId) {
+    try {
+      return $this->apps->get($appId);
+    }
+    catch (Exception $e) {
+      // TODO: Handle exceptions.
+    }
+  }
+
+  /**
+   * Assigns a specific user to an app in Okta.
+   *
+   * @param string $appId
+   *   The App ID.
+   * @param array $users
+   *   An associative array containing the user's credentials
+   *   and optionally a profile. Example at:
+   *   https://developer.okta.com/docs/api/resources/apps.html#request-example-23.
+   *
+   * @return object
+   *   The response.
+   */
+  public function assignUsersToApp($appId, array $users) {
+    try {
+      return $this->apps->assignUser($appId, $users);
+    }
+    catch (Exception $e) {
+      // TODO: Handle exceptions.
+    }
+  }
+
 }
