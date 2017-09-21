4feb8d85d5403d12702613d387a03583a4d830c3
Stuart Carruthers <stuartcarruthers@3543145.no-reply.drupal.org>
2017-09-21 13:31:03 +0100

    Remove try/catch from OktaClient constructor

--- src/Service/OktaClient.php
+++ src/Service/OktaClient.php
@@ -3,7 +3,6 @@
 namespace Drupal\okta_api\Service;
 
 use Okta\Client;
-use Okta\Exception as OktaException;
 use Drupal\Core\Config\ConfigFactory;
 
 /**
@@ -17,31 +16,22 @@ class OktaClient {
    * Create the Okta API client.
    */
   public function __construct(ConfigFactory $config_factory) {
-    // Get the config.
     $config = $config_factory->get('okta_api.settings');
 
-    try {
-      $this->Client = new Client(
+    $this->Client = new Client(
         $config->get('organisation_url'),
         $config->get('api_key'),
         [
           // Don't auto-bootstrap the Okta resource properties.
-          'bootstrap' => false,
+          'bootstrap' => FALSE,
           // Use the okta preview (oktapreview.com) domain.
           'preview' => $config->get('preview_domain'),
-          //'headers' => [
-          //  'Some-Header'    => 'Some value',
-          //  'Another-Header' => 'Another value'
-          //]
+          // 'headers' => [
+          // 'Some-Header'    => 'Some value',
+          // 'Another-Header' => 'Another value'
+          // ].
         ]
       );
-    }
-    catch (OktaException $e) {
-      // TODO handle exceptions.
-      //ksm($e);
-      //\Drupal::logger('okta_api')->warning("Failed to create Okta Client using API: @message",
-      //  ['@message' => $e->getMessage()]);
-    }
   }
 
 }
